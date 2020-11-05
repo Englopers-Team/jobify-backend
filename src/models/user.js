5e98624ed7eb89d0ff44653930e4508178b611e4
AbdulhakimZatar <hakimzatar@gmail.com>
2020-11-05 21:41:20 +0200

    setup routers

---
+++ src/models/user.js
@@ -0,0 +1,50 @@
+'use strict';
+
+class User{
+  constructor(){
+
+  }
+
+  dashboard(user){
+    // get statistics for the user and recommanded jobs // only five jobs
+  }
+
+  applyDB(user,jobID){
+
+  }
+
+  applyAPI(user,email){
+
+  }
+
+  userApps(user){
+
+  }
+
+  deleteApp(appID){
+
+  }
+
+  userOffers(user){
+
+  }
+
+  answerOffer(offerID,payload){
+
+  }
+
+  editProfile(user,payload){
+    
+  }
+
+  searchJob(payload){
+
+  }
+  
+  searchCompany(payload){
+    
+  }
+
+}
+
+module.exports = new User();
--- src/server.js
+++ src/server.js
@@ -3,8 +3,33 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const notFoundHandler = require('./middleware/error/404');
+const errorHandler = require('./middleware/error/500');
 const app = express();
 app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(express.static('./public'));
+
+const communityRouter = require('./routes/community');
+const companyRouter = require('./routes/company');
+const homepageRouter = require('./routes/homepage');
+const searchRouter = require('./routes/search');
+const userRouter = require('./routes/user');
+const authRouter = require('./routes/auth');
+const adminRouter = require('./routes/admin');
+
+app.use('/', homepageRouter);
+app.use('/', authRouter);
+app.use('/company', companyRouter);
+app.use('/user', userRouter);
+app.use('/search', searchRouter);
+app.use('/community', communityRouter);
+app.use('/admin', adminRouter);
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
 
 module.exports = {
   server: app,
