e54c4ddccce2ee9fc36b4f7a823b86ac7e2b0629
Abdallah-Zakaria <abdallah.zakaria1996@icloud.com>
2020-11-08 16:10:52 +0200

    Abdallah community method/routes for admin and users

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -4,21 +4,12 @@ const router = express.Router();
 const admin = require('../models/admin');
 const helper = require('../models/helper');
 const authorize = require('../middleware/auth/authorization/authorize');
+const community = require('../models/community');
 
 router.get('/', async (req, res) => {
   const data = await admin.dashboard();
   res.status(200).json(data);
 });
-
-router.post('/block/:id', (req, res) => {});
-
-router.get('/posts', (req, res) => {});
-
-router.delete('/posts/:id', (req, res) => {});
-
-router.delete('/comments/:id', (req, res) => {});
-
-// router.post('/approve/:id', (req, res) => {});
 
 router.get('/report', async (req, res) => {
   const data = await admin.reports();
@@ -44,6 +35,26 @@ router.post('/seed/:id', authorize(['admin']), async (req, res) => {
 router.delete('/report/:id', authorize(['admin']), async (req, res) => {
   await admin.deleteReport(req.params.id);
   res.status(202).json({});
+});
+
+router.get('/posts', async (req, res) => {
+  const data = await community.posts(req.user);
+  res.status(200).json(data);
+});
+
+router.get('/posts/:id', async (req, res) => {
+  const data = await community.getPost(req.params.id);
+  res.status(200).json(data);
+});
+
+router.patch('/posts/:id', async (req, res) => {
+  await community.pin(req.params.id, req.body.response);
+  res.status(201).json({});
+});
+
+router.delete('/posts/:id', async (req, res) => {
+  await community.deletePost(req.user.id, req.params.id);
+  res.status(201).json({});
 });
 
 module.exports = router;
