f4f628adafe178ea061dd803f3a8f0666eebdff6
AbdulhakimZatar <hakimzatar@gmail.com>
2020-11-10 19:41:31 +0200

    fix tests

--- src/middleware/auth/authorization/authorize.js
+++ src/middleware/auth/authorization/authorize.js
@@ -1,7 +1,7 @@
 'use strict';
 const authHelpers = require('../../../models/auth-helpers');
 
-module.exports = async (role) => {
+module.exports = (role) => {
   async (req, res, next) => {
     try {
       if (!req.cookies.token) {
