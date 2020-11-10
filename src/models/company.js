4d1e794e5c75c63f8ef8ad5c425c57c544d3579d
AbdulhakimZatar <hakimzatar@gmail.com>
2020-11-10 13:42:50 +0200

    company tests

--- src/models/company.js
+++ src/models/company.js
@@ -6,7 +6,7 @@ const notifi = require('../models/notifications');
 const helper = require('./helper');
 
 class Company {
-  constructor() {}
+  constructor() { }
 
   async dashboard(company) {
     const offers = await this.companyOffers(company);
@@ -15,7 +15,7 @@ class Company {
   }
 
   async jobs(company) {
-    const id = helper.getID(company.id, 'company');
+    const id = await helper.getID(company.id, 'company');
     let SQL = `SELECT * FROM jobs WHERE company_id=$1;`;
     let value = [id];
     const data = await client.query(SQL, value);
@@ -23,44 +23,68 @@ class Company {
   }
 
   async submitJob(company, payload) {
-    const id = helper.getID(company.id, 'company');
+    const id = await helper.getID(company.id, 'company');
     let { title, location, type, description } = payload;
     let SQL = `INSERT INTO jobs (company_id,title,location,type,description) VALUES ($1,$2,$3,$4,$5);`;
     let value = [id, title, location, type, description];
     await client.query(SQL, value);
   }
 
-  async editJob(jobID, payload) {
-    let { title, location, type, description } = payload;
-    let SQL = `UPDATE jobs SET title=$1,location=$2,type=$3,description=$4 WHERE id=$5;`;
-    let value = [title, location, type, description, jobID];
-    await client.query(SQL, value);
+  async editJob(company, jobID, payload) {
+    const id = await helper.getID(company.id, 'company');
+    const SQL1 = `SELECT company_id FROM jobs WHERE id=$1`;
+    const value1 = [jobID];
+    const check = await client.query(SQL1, value1);
+    if (check.rows[0].company_id == id) {
+      let { title, location, type, description } = payload;
+      let SQL = `UPDATE jobs SET title=$1,location=$2,type=$3,description=$4 WHERE id=$5;`;
+      let value = [title, location, type, description, jobID];
+      await client.query(SQL, value);
+    } else {
+      throw new Error(`Can't edit job`);
+    }
   }
 
-  async deleteJob(jobID) {
-    let SQL1 = `DELETE FROM applications WHERE job_id=$1;`;
-    let SQL2 = `DELETE FROM jobs WHERE id=$1;`;
+  async deleteJob(company, jobID) {
+    const id = await helper.getID(company.id, 'company');
     let value = [jobID];
-    await client.query(SQL1, value);
-    await client.query(SQL2, value);
+    const SQL0 = `SELECT company_id FROM jobs WHERE id=$1`;
+    const check = await client.query(SQL0, value);
+    if (check.rows[0].company_id == id) {
+      let SQL1 = `DELETE FROM applications WHERE job_id=$1;`;
+      let SQL2 = `DELETE FROM jobs WHERE id=$1;`;
+      await client.query(SQL1, value);
+      await client.query(SQL2, value);
+    } else {
+      throw new Error(`Can't delete job`);
+    }
   }
 
   async companyApps(company) {
-    const id = helper.getID(company.id, 'company');
+    const id = await helper.getID(company.id, 'company');
     let SQL = `SELECT * FROM applications WHERE company_id=$1`;
     let value = [id];
     const data = await client.query(SQL, value);
     return data.rows;
   }
 
-  async answerApp(appID, payload) {
-    let SQL = `UPDATE applications SET status=$1 WHERE id=$2`;
-    let value = [payload, appID];
-    await client.query(SQL, value);
+  async answerApp(company, appID, payload) {
+    const id = await helper.getID(company.id, 'company');
+    let values = [payload, appID];
+    const value = [appID];
+    const SQL0 = `SELECT company_id FROM applications WHERE id=$1;`;
+    const check = await client.query(SQL0, value);
+    if (check.rows[0].company_id == id) {
+      let SQL = `UPDATE applications SET status=$1 WHERE id=$2;`;
+      await client.query(SQL, values);
+    } else {
+      throw new Error(`Can't answer application`);
+    }
+
   }
 
   async companyOffers(company) {
-    const id = helper.getID(company.id, 'company');
+    const id = await helper.getID(company.id, 'company');
     let SQL = `SELECT * FROM job_offers WHERE company_id=$1`;
     let value = [id];
     const data = await client.query(SQL, value);
@@ -80,14 +104,23 @@ class Company {
     await notifi.addNotification(data);
   }
 
-  async deleteOffer(offerID) {
-    let SQL = `DELETE FROM job_offers WHERE id=$1`;
-    let value = [offerID];
-    await client.query(SQL, value);
+  async deleteOffer(company, offerID) {
+    const id = await helper.getID(company.id, 'company');
+    const value0 = [offerID];
+    const SQL0 = `SELECT company_id FROM job_offers WHERE id=$1;`;
+    const check = await client.query(SQL0, value0);
+    if (check.rows[0].company_id == id) {
+      let SQL = `DELETE FROM job_offers WHERE id=$1`;
+      let value = [offerID];
+      await client.query(SQL, value);
+    } else {
+      throw new Error(`Can't answer application`);
+    }
+
   }
 
   async editProfile(company, payload) {
-    const id = helper.getID(company.id, 'company');
+    const id = await helper.getID(company.id, 'company');
     let { company_name, phone, logo, country, company_url } = payload;
     let SQL = `UPDATE company SET company_name=$1,phone=$2,logo=$3,country=$4,company_url=$5 WHERE id=$6;`;
     let value = [company_name, phone, logo, country, company_url, id];
--- src/routes/company.js
+++ src/routes/company.js
@@ -8,14 +8,22 @@ router.get('/jobs', async (req, res) => {
   res.status(200).json(data);
 });
 
-router.put('/jobs/:id', async (req, res) => {
-  await company.editJob(req.params.id, req.body);
-  res.status(202).json({});
+router.put('/jobs/:id', async (req, res, next) => {
+  try {
+    await company.editJob(req.user, req.params.id, req.body);
+    res.status(202).json({});
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete('/jobs/:id', async (req, res) => {
-  await company.deleteJob(req.params.id);
-  res.status(202).json({});
+router.delete('/jobs/:id', async (req, res, next) => {
+  try {
+    await company.deleteJob(req.user, req.params.id);
+    res.status(202).json({});
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post('/submit', async (req, res) => {
@@ -28,9 +36,13 @@ router.get('/app', async (req, res) => {
   res.status(200).json(data);
 });
 
-router.put('/app/:id', async (req, res) => {
-  await company.answerApp(req.params.id, req.body.status);
-  res.status(202).json({});
+router.put('/app/:id', async (req, res, next) => {
+  try {
+    await company.answerApp(req.user, req.params.id, req.body.status);
+    res.status(202).json({});
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.get('/offers', async (req, res) => {
@@ -43,9 +55,13 @@ router.post('/offers/:id', async (req, res) => {
   res.status(201).json({});
 });
 
-router.delete('/offers/:id', async (req, res) => {
-  await company.deleteOffer(req.params.id);
-  res.status(202).json({});
+router.delete('/offers/:id', async (req, res,next) => {
+  try {
+    await company.deleteOffer(req.user, req.params.id);
+    res.status(202).json({});
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.put('/edit', async (req, res) => {
