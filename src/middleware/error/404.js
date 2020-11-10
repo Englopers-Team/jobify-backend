080738c5ff4a83ea305bc165b0b834c626fb6bc2
Othabteh <o.thabteh@gmail.com>
2020-11-11 00:02:31 +0200

    osama-deplymet app

--- src/middleware/error/404.js
+++ src/middleware/error/404.js
@@ -1,7 +1,7 @@
 'use strict';
 
-//-----------------------------------// Export Module \\-----------------------------------\\
-module.exports = (req,res,next) =>{
+//-----------------------------------// Export The Module \\-----------------------------------\\
+module.exports = (req, res, next) => {
   res.status(404);
   res.statusMessage = 'Not Found';
   res.json({ error: 'Page not found' });
