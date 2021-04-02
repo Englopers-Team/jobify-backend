97174294d39351816bc096bd37f3cc8bad521ebf
messeili <mymoa.2014@gmail.com>
2021-04-02 19:37:14 +0300

    fix mongo

--- index.js
+++ index.js
@@ -9,20 +9,25 @@ const client = require('./src/models/database');
 const server = require('./src/server');
 
 //--------------------------------// Esoteric Resources \\-------------------------------\\
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://heroku:heroku1337@cluster0.1i1ry.mongodb.net/jobify?retryWrites=true&w=majority' || 'mongodb://localhost:27017/jobify';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://messeili:MOMO0000@cluster0.zibi1.mongodb.net/jobify?retryWrites=true&w=majority' || 'mongodb://localhost:27017/jobify';
 const PORT = process.env.PORT;
 
 //--------------------------------// Server Connection \\--------------------------------\\
-mongoose.connect(MONGODB_URI, {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(()=>{
-  client.connect().then(() => {
-    server.start(PORT);
-  }).catch((err) => {
-    console.error(err.message);
+mongoose
+  .connect(MONGODB_URI, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    client
+      .connect()
+      .then(() => {
+        server.start(PORT);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
   });
-});
 
 //---------------------------------------------------------------------------------------\\
